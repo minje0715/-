fe166e2e9ea4df79457f34c4c8c7fc0256105d63
minje0715 <kmjo07815@naver.com>
2022-06-28 17:00:20 +0900

    회원제 게시판 시작

--- src/main/java/com/its/memberboardproject/service/MemberService.java
+++ src/main/java/com/its/memberboardproject/service/MemberService.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +22,15 @@ public class MemberService {
         String memberFileName = memberProfile.getOriginalFilename();
         memberFileName = System.currentTimeMillis() + "_" + memberFileName;
         String savePath = "C:\\springboot_img\\" + memberFileName;
+        if (!memberProfile.isEmpty()) {
+            memberProfile.transferTo(new File(savePath));
+        }
+        memberDTO.setMemberFileName(memberFileName);
 
-
+        Optional<MemberEntity> optionalMemberEntity =
+                memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(optionalMemberEntity.isPresent()) {
+            Mem
+        }
     }
 }
